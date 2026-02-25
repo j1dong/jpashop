0adafa3f8f48f1c48742c56483f3aff06e7c4866
j1dong <gtgsky2022@gmail.com>
2026-02-25 15:52:42 +0900

    Item 엔티티에 재고 증가 및 감소 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -25,4 +25,16 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
 
+    //비즈니스 로직
+    public void addStock(int quantity) { //상품 재고 수량 증가 로직
+        this.stockQuantity += quantity; //현재 재고 + 추가 수량
+    }
+
+    public void removeStock(int quantity) { //상품 재고 수량 감소 로직
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) { //현재 재고보다 더 많이 감소하면 안됨
+            throw new NotEnoughStockException("need more stock"); //더 많이 감소 시 예외 발생!
+        }
+        this.stockQuantity = restStock; //예외 발생 없을 시 정상적으로 재고 감소
+    }
 }

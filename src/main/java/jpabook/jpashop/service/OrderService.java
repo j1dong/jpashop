5e65e825994b53e3c16af0acf699c3c246179028
j1dong <gtgsky2022@gmail.com>
2026-02-26 15:58:03 +0900

    OrderService 생성

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    //주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) { //주문하는 회원 id, 상품 id, 주문 수량 정보를 받아 실제 주문 엔티티를 생성한 후 저장
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    //주문 취소
+    @Transactional
+    public void cancelOrder(Long orderId) { //주문 식별자를 받아 주문 엔티티를 조회한 후 주문 엔티티에 주문 취소를 요청
+
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        //주문 취소
+        order.cancel();
+    }
+
+    //주문 검색
+    /*
+    public List<Order> findOrders(OrderSearch orderSearch) { //OrderSearch 라는 검색 조건을 가진 객체로 주문 엔티티를 검색
+        return orderRepository.findAll(orderSearch);
+    }
+     */
+
+}
